1b32ad5e497fed254773472bb06f2012cb5a7040
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-05-28 08:50:46 +0200

    test: new samples
    Added new test samples for the library: no API program and EGL program

---
+++ test/02-no-api.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#
+#include <string.h>
+#include <X11/Xlib.h>
+#
+#define WINDOW_IMPLEMENTATION
+#include "./../window.h"
+
+int main(void) {
+    /* initialize window.h */
+    win_init();
+   
+    /* create window */
+    t_window window = 0;
+    win_create(&window, 800, 600, "02 - no API", 0);
+    if (!window) {
+        return (1);
+    }
+    win_map(window);
+
+    /* query properties */
+    Display *dpy = win_getWindowProperty(window, WINDOW_PROP_WINDOW_X11_DISPLAY);
+    XID w_id = *(XID *) win_getWindowProperty(window, WINDOW_PROP_WINDOW_X11_WINDOW_ID);
+    Visual *visual = win_getWindowProperty(window, WINDOW_PROP_WINDOW_X11_VISUAL);
+
+    /* create buffer */
+    uint32_t *buffer = malloc(800 * 600 * sizeof(uint32_t));
+    if (!buffer) {
+        return (1);
+    }
+
+    /* create bitmap */
+    XImage *bitmap = XCreateImage(dpy, visual, 24, ZPixmap, 0, (char *) buffer, 800, 600, 32, 800 * sizeof(uint32_t));
+    if (!bitmap) {
+        return (1);
+    }
+
+    /* create graphics context */
+    GC gc = XCreateGC(dpy, w_id, 0, 0);
+
+    int exit = 0;
+    while (!exit) {
+        /* set the background color */
+        for (size_t i = 0; i < 800 * 600; i++) {
+            buffer[i] = 0xff191919; /* equivalent of: (1.0f, 0.1, 0.1, 0.1) */
+        }
+
+        /* display */
+        XPutImage(dpy, w_id, gc, bitmap, 0, 0, 0, 0, 800, 600);
+
+        /* poll events */
+        t_event event = { 0 };
+        while (win_pollEvents(&event)) {
+            switch (event.type) {
+                case (WINDOW_EVENT_QUIT): {
+                    printf("WINDOW_EVENT_QUIT\n");
+                    exit = 1;
+                } break;
+            }
+        }
+    }
+
+    /* quit */
+    XFreeGC(dpy, gc), gc = 0;
+    XDestroyImage(bitmap), bitmap = 0;
+    win_destroy(window), window = 0;
+    win_quit();
+    return (0);
+}
---
+++ test/03-egl.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#
+#include <GL/gl.h>
+#include <EGL/egl.h>
+#
+#define WINDOW_IMPLEMENTATION
+#include "./../window.h"
+
+/* egl context struct */
+static struct {
+    EGLDisplay dpy;
+    EGLSurface surface;
+    EGLConfig  cfg;
+    EGLContext ctx;
+} egl = { 0 };
+
+/* egl config attributes */
+static const EGLint cfg_attr[] = {
+    EGL_SURFACE_TYPE,     EGL_WINDOW_BIT,
+    EGL_RED_SIZE,         8,
+    EGL_GREEN_SIZE,       8,
+    EGL_BLUE_SIZE,        8,
+    EGL_RENDERABLE_TYPE,  EGL_OPENGL_BIT,
+    EGL_NONE
+};
+
+/* egl context attributes */
+static const EGLint ctx_attr[] = {
+    EGL_CONTEXT_MAJOR_VERSION, 4,
+    EGL_CONTEXT_MINOR_VERSION, 6,
+    EGL_NONE
+};
+
+int main(void) {
+    /* initialize window.h */
+    win_init();
+   
+    /* create window */
+    t_window window = 0;
+    win_create(&window, 800, 600, "03 - egl", 0);
+    win_map(window);
+    
+    /* query EGL display */
+    Display *dpy = win_getWindowProperty(window, WINDOW_PROP_WINDOW_X11_DISPLAY);
+    egl.dpy = eglGetDisplay((EGLDisplay) dpy);
+    if (egl.dpy == EGL_NO_DISPLAY) {
+        return (1);
+    }
+
+    /* initialzie EGL */
+    if (!eglInitialize(egl.dpy, 0, 0)) { return (1); }
+    if (!eglBindAPI(EGL_OPENGL_API))   { return (1); }
+
+    /* query EGL config */
+    EGLint cfg_cnt = 0;
+    if (!eglChooseConfig(egl.dpy, cfg_attr, &egl.cfg, 1, &cfg_cnt)) { return (1); }
+    if (cfg_cnt != 1) {
+        return (1);
+    }
+
+    /* create EGL surface */
+    XID w_id = *(XID *) win_getWindowProperty(window,WINDOW_PROP_WINDOW_X11_WINDOW_ID);
+    egl.surface = eglCreateWindowSurface(egl.dpy, egl.cfg, w_id, 0);
+    if (egl.surface == EGL_NO_SURFACE) {
+        return (1);
+    }
+
+    /* create EGL context */
+    egl.ctx = eglCreateContext(egl.dpy, egl.cfg, EGL_NO_CONTEXT, ctx_attr);
+    if (egl.ctx == EGL_NO_CONTEXT) {
+        return (1);
+    }
+
+    /* make context current */
+    if (!eglMakeCurrent(egl.dpy, egl.surface, egl.surface, egl.ctx)) {
+        return (1);
+    }
+
+    printf("GL_VERSION: %s\n", glGetString(GL_VERSION));
+
+    int exit = 0;
+    while (!exit) {
+        /* render */
+        glClearColor(0.1, 0.1, 0.1, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        eglSwapBuffers(egl.dpy, egl.surface);
+
+        /* poll events */
+        t_event event = { 0 };
+        while (win_pollEvents(&event)) {
+            switch (event.type) {
+                case (WINDOW_EVENT_QUIT): {
+                    printf("WINDOW_EVENT_QUIT\n");
+                    exit = 1;
+                } break;
+            }
+        }
+    }
+
+    /* quit */
+    eglMakeCurrent(egl.dpy, 0, 0, 0);
+    eglDestroyContext(egl.dpy, egl.ctx);
+    eglDestroySurface(egl.dpy, egl.surface);
+    eglTerminate(egl.dpy);
+
+    win_destroy(window), window = 0;
+    win_quit();
+    return (0);
+}
