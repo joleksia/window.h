b42fabd9ccdad9b65aa9a03220400ae7ba43b017
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-08-02 19:02:43 +0200

    samples: updated samples

--- samples/01-basic.c
+++ samples/01-basic.c
@@ -8,56 +8,33 @@ int main(void) {
     library_t library = 0;
     winInit(&library);
 
+    /* set current API as 'Native' */
     winSetHints(library, WINDOW_CLIENT_API, WINDOW_API_NATIVE);
 
+    /* create window */
     window_t window = 0;
     winCreateWindow(library, &window, 800, 600, "Hello, window.h - Sample 01. Basic");
-    winMapWindow(library, window);
+    if (!window) { winQuit(library); return (1); }
 
+    /* create context */
     context_t context = 0;
     winCreateContext(library, &context, window);
+    if (!context) { winQuit(library); return (2); }
 
+    /* map window */
+    winMapWindow(library, window);
+
+    /* update loop */
     int exit = 0;
     while (!exit) {
         event_t event = { 0 };
         while (winPollEvents(library, &event)) {
             /* wait for 'quit' event */
             if (event.type == WINDOW_EVENT_QUIT) { exit = 1; }
-
-            /* output the event name to stdout */
-            if (event.type == WINDOW_EVENT_NONE) { printf("WINDOW_EVENT_NONE\n"); }
-            
-            else if (event.type == WINDOW_EVENT_QUIT) { printf("WINDOW_EVENT_QUIT\n"); }
-            
-            else if (event.type == WINDOW_EVENT_MOUSE_MOTION) { printf("WINDOW_EVENT_MOUSE_MOTION\n"); }
-            else if (event.type == WINDOW_EVENT_MOUSE_BUTTON) { printf("WINDOW_EVENT_MOUSE_BUTTON\n"); }
-            else if (event.type == WINDOW_EVENT_MOUSE_SCROLL) { printf("WINDOW_EVENT_MOUSE_SCROLL\n"); }
-            else if (event.type == WINDOW_EVENT_MOUSE_ADDED) { printf("WINDOW_EVENT_MOUSE_ADDED\n"); }
-            else if (event.type == WINDOW_EVENT_MOUSE_REMOVED) { printf("WINDOW_EVENT_MOUSE_REMOVED\n"); }
-            
-            else if (event.type == WINDOW_EVENT_KEYBOARD_KEY) { printf("WINDOW_EVENT_KEYBOARD_KEY\n"); }
-            else if (event.type == WINDOW_EVENT_KEYBOARD_ADDED) { printf("WINDOW_EVENT_KEYBOARD_ADDED\n"); }
-            else if (event.type == WINDOW_EVENT_KEYBOARD_REMOVED) { printf("WINDOW_EVENT_KEYBOARD_REMOVED\n"); }
-            
-            else if (event.type == WINDOW_EVENT_WINDOW_CREATE) { printf("WINDOW_EVENT_WINDOW_CREATE\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_DESTROY) { printf("WINDOW_EVENT_WINDOW_DESTROY\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_MAP) { printf("WINDOW_EVENT_WINDOW_MAP\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_UNMAP) { printf("WINDOW_EVENT_WINDOW_UNMAP\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_RESIZE) { printf("WINDOW_EVENT_WINDOW_RESIZE\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_MOTION) { printf("WINDOW_EVENT_WINDOW_MOTION\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_ENTER) { printf("WINDOW_EVENT_WINDOW_ENTER\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_LEAVE) { printf("WINDOW_EVENT_WINDOW_LEAVE\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_MAXIMIZE) { printf("WINDOW_EVENT_WINDOW_MAXIMIZE\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_MINIMIZE) { printf("WINDOW_EVENT_WINDOW_MINIMIZE\n"); }
-            else if (event.type == WINDOW_EVENT_WINDOW_FULLSCREEN) { printf("WINDOW_EVENT_WINDOW_FULLSCREEN\n"); }
-
-            else if (event.type == WINDOW_EVENT_SELECTION_COPY) { printf("WINDOW_EVENT_SELECTION_COPY\n"); }
-            else if (event.type == WINDOW_EVENT_SELECTION_PASTE) { printf("WINDOW_EVENT_SELECTION_PASTE\n"); }
-            else if (event.type == WINDOW_EVENT_SELECTION_CLEAR) { printf("WINDOW_EVENT_SELECTION_CLEAR\n"); }
         }
     }
 
-    /* quit window.h */
+    /* quit */
     winQuit(library);
     return (0);
 }
--- samples/02-opengl.c
+++ samples/02-opengl.c
@@ -9,15 +9,20 @@ int main(void) {
     library_t library = 0;
     winInit(&library);
 
+    /* set current API as 'OpenGL' */
     winSetHints(library, WINDOW_CLIENT_API, WINDOW_API_OPENGL);
+
+    /* configure OpenGL hints */
     winSetHints(library, WINDOW_GL_CONTEXT_VERSION_MAJOR, 1);
     winSetHints(library, WINDOW_GL_CONTEXT_VERSION_MINOR, 0);
     winSetHints(library, WINDOW_GL_CONTEXT_PROFILE, WINDOW_GL_CONTEXT_PROFILE_COMPATIBILITY);
 
+    /* create window */
     window_t window = 0;
     winCreateWindow(library, &window, 800, 600, "Hello, window.h - Sample 02. OpenGL");
     if (!window) { winQuit(library); return (1); }
 
+    /* create context */
     context_t context = 0;
     winCreateContext(library, &context, window);
     if (!context) { winQuit(library); return (2); }
@@ -26,8 +31,13 @@ int main(void) {
     winGLMakeCurrent(library, context);
     winGLSwapInterval(library, context, 1);
 
-    printf("%s\n", glGetString(GL_VERSION));
+    /* print OpenGL info */
+    printf("- Vendor: %s\n", glGetString(GL_VENDOR));
+    printf("- Version: %s\n", glGetString(GL_VERSION));
+    printf("- Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("- GLSL Version: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
+    /* update-render loop */
     int exit = 0;
     while (!exit) {
         /* render */
@@ -40,11 +50,12 @@ int main(void) {
             glVertex2f( 0.5,-0.5);
         glEnd();
 
-        /* poll events */
         if (!winGLSwapBuffers(library, context)) {
             /* 'winGLSwapBuffers' failed: fall-back to 'glFlush' */
             glFlush();
         }
+
+        /* poll events */
         event_t event = { 0 };
         while (winPollEvents(library, &event)) {
             switch (event.type) {
